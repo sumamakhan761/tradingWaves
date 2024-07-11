ce4f9e3370acf60b798d34bbc00b03daf71b88a6
zarodah <sumama761>
2024-07-11 14:37:38 +0530

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,7 +9,7 @@ import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
-import { GeneralContextProvider } from "./GeneralContext";
+
 
 const Dashboard = () => {
   return (
@@ -22,7 +22,6 @@ const Dashboard = () => {
           <Route path="/holdings" element={<Holdings />} />
           <Route path="/positions" element={<Positions />} />
           <Route path="/funds" element={<Funds />} />
-          <Route path="/apps" element={<Apps />} />
         </Routes>
       </div>
     </div>
